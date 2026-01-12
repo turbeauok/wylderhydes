aeab7a67628e5c939100b880a6cc191d4487bc88
turbeauok <turbo911.2000@gmail.com>
2026-01-12 19:39:21 +0000

    Update javaskriptas.js

--- js/javaskriptas.js
+++ js/javaskriptas.js
@@ -3,7 +3,7 @@ import { UserManager } from "oidc-client-ts";
 const cognitoAuthConfig = {
     authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_eYVKPnrl1",
     client_id: "7u3fgqn801unoj8gd7fp0s6gq2",
-    redirect_uri: "https://master.d108d7anayy6gv.amplifyapp.com/dredir.html" //"https://d84l1y8p4kdic.cloudfront.net",
+    redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",//"https://master.d108d7anayy6gv.amplifyapp.com/dredir.html", //"https://d84l1y8p4kdic.cloudfront.net",
     response_type: "code",
     scope: "email openid phone"
 };
